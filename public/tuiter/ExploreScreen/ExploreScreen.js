267db90473608a1c3cf53a9d082eb667a9f48496
Heekyung Kim <heekyung.kim27@gmail.com>
2022-02-23 19:31:29 -0500

    Fix directory linking error

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,17 +1,21 @@
-import NavigationSidebar from "../NavigationSidebar/index";
-import ExploreComponent from "./ExploreComponent.js";
-import WhoToFollowList from "../WhoToFollowList.js";
+import NavigationSidebar from "../NavigationSidebar/index.js";
+// import ExploreComponent from "./ExploreComponent.js";
+// import WhoToFollowList from "../WhoToFollowList.js";
 
 (function ($) {
     $('#wd-explore').append(`
         <div class="row mt-2">
-        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-            ${NavigationSidebar()}
-        </div>
-            ${ExploreComponent()}
-        <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-            ${WhoToFollowList()}
-        </div>
+            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                ${NavigationSidebar()}
+            </div>
         </div>
     `);
 })($);
+
+
+// <div class="col-xxl-10 col-xl-10 d-xl-block d-lg-none d-md-none d-sm-none">
+// ${ExploreComponent()}
+// </div>
+// <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+// ${WhoToFollowList()}
+// </div>

1d21ec3667248c51973f1bf51d60bf6651420d02
Heekyung Kim <heekyung.kim27@gmail.com>
2022-03-28 11:59:04 -0400

    Finish a7-tuiter-inputField

--- src/components/tuiter/HomeScreen/WhatsHappening.js
+++ src/components/tuiter/HomeScreen/WhatsHappening.js
@@ -9,11 +9,27 @@ const WhatsHappening = () => {
   }
     return (
     <>
-        <textarea value={whatsHappening} onChange={(event) => setWhatsHappening(event.target.value)}>
-        </textarea>
-        <button onClick={tuitClickHandler}>
-            Tuit
-        </button>
+        <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2 mt-1 text-center">
+            <img src="https://pbs.twimg.com/media/E9s0Z08UYAIxtI7?format=jpg" alt="" className="img-fluid profile-pic"/>
+        </div>
+        <div className="fg-white col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-10 mt-1">
+            <textarea className="p-2" style={{background:"transparent", width:"100%", height:"50%", border:"solid rgba(211, 211, 211, 0.219) 1px"}} placeholder="What's happening?" value={whatsHappening} onChange={(event) => setWhatsHappening(event.target.value)}>
+            </textarea>
+            
+            <div className="row mt-2 mb-2">
+              <div className='col-10' style={{color:"#5bc0de"}}>
+                <i className="fa fa-image m-1"></i>
+                <i className="fa fa-chart-area m-1"></i>
+                <i className="fa fa-smile m-1"></i>
+                <i className="fa fa-calendar m-1"></i>
+              </div>
+              <div className="col-2 float-right pb-2">
+                <button className="btn btn-primary btn-block rounded-pill" onClick={tuitClickHandler}> Tweet </button>
+              </div>
+            </div> 
+
+        </div>
+
     </>
   )
 }

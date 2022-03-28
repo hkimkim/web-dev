145a4b9ca3dbbda9fbe7f423e0d389e73c3ad324
Heekyung Kim <heekyung.kim27@gmail.com>
2022-03-28 09:47:17 -0400

    Finish a7-tuiter-creatingTuits

--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,7 +1,29 @@
 import tuits from "../data/tuits.json"
 
-const tuitsReducer = (state = tuits) => {
-  return (state)
-}
+const tuitsReducer = (state = tuits, action) => {
+  switch (action.type) {
+    case 'create-tuit':
+      const newTuit = {
+        tuit: action.tuit,
+        _id: (new Date()).getTime() + '',
+        postedBy: {
+          username: "ReactJS"
+        },
+        handle: "ReactJS",
+        stats: {
+          retuits: 111,
+          likes: 222,
+          comments: 333
+        },
+        logo_image: "https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png",
 
+      }
+      return [
+        newTuit,
+        ...state,
+      ];
+    default:
+      return tuits
+  }
+} 
 export default tuitsReducer;
